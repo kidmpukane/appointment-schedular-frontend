d538d5a7ead487d0375a3a6f734dbee4c78bf968
kidmpukane <lwandisozwane91@gmail.com>
2024-06-18 14:15:01 +0200

    basic styling complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,7 +20,7 @@ const monthNames = [
   "December",
 ];
 
-const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const getDaysInMonth = (year, monthIndex) => {
   const firstDayOfMonth = new Date(year, monthIndex, 1).getDay();
@@ -48,7 +48,6 @@ const getDaysInMonth = (year, monthIndex) => {
   return dates;
 };
 
-// Function to format time from HH:mm:ss to hh:mm AM/PM
 const formatTime = (timeString) => {
   const [hours, minutes] = timeString.split(":").slice(0, 2).map(Number);
   const period = hours >= 12 ? "PM" : "AM";
@@ -56,23 +55,6 @@ const formatTime = (timeString) => {
   const formattedHours = String(displayHours).padStart(2, "0");
   const formattedMinutes = String(minutes).padStart(2, "0");
   return `${formattedHours}:${formattedMinutes} ${period}`;
-};
-
-const handleDeleteAppointment = (appointmentId) => {
-  axios
-    .delete(
-      `http://127.0.0.1:8000/appointments/delete-appointment/${appointmentId}/`
-    )
-    .then((response) => {
-      // Handle successful deletion (e.g., update state or show message)
-      console.log("Appointment deleted successfully:", response.data);
-      // Example: Remove the deleted appointment from state
-      setData(data.filter((appointment) => appointment.id !== appointmentId));
-    })
-    .catch((error) => {
-      console.error("Error deleting appointment:", error);
-      // Handle error (e.g., show error message)
-    });
 };
 
 const HomePage = () => {
@@ -131,6 +113,20 @@ const HomePage = () => {
     return data.some((meeting) => isSameDay(new Date(meeting.date), date));
   };
 
+  const handleDeleteAppointment = (appointmentId) => {
+    axios
+      .delete(
+        `http://127.0.0.1:8000/appointments/delete-appointment/${appointmentId}/`
+      )
+      .then((response) => {
+        console.log("Appointment deleted successfully:", response.data);
+        setData(data.filter((appointment) => appointment.id !== appointmentId));
+      })
+      .catch((error) => {
+        console.error("Error deleting appointment:", error);
+      });
+  };
+
   return (
     <div className="calendar-container-home">
       <div className="calendar-view-home">
@@ -141,7 +137,6 @@ const HomePage = () => {
           <button className="prev-month-button" onClick={previousMonth}>
             <ArrowBackIosIcon fontSize="small" />
           </button>
-
           <button className="next-month-button" onClick={nextMonth}>
             <ArrowForwardIosIcon fontSize="small" />
           </button>
@@ -166,11 +161,11 @@ const HomePage = () => {
               const isToday = isSameDay(dayDate, new Date());
               const hasMeetingDot = hasMeeting(dayDate);
               const dayButtonClass = `
-              day-button
-              ${isSelected ? "selected-day" : ""}
-              ${isToday ? "today" : ""}
-              ${isCurrentMonth ? "" : "outside-month"}
-            `;
+                day-button
+                ${isSelected ? "selected-day" : ""}
+                ${isToday ? "today" : ""}
+                ${isCurrentMonth ? "" : "outside-month"}
+              `;
 
               return (
                 <div key={index} className="day-container">
@@ -189,49 +184,46 @@ const HomePage = () => {
       </div>
       <div className="schedule-view">
         <h2 className="schedule-title">Appointments</h2>
-
-        <div className="task-card">
-          <ul className="meeting-list">
-            {data
-              .filter((meeting) =>
-                isSameDay(new Date(meeting.date), selectedDay)
-              )
-              .map((meeting) => (
-                <li key={meeting.id} className="meeting-item">
-                  <div className="meeting-details">
-                    <div className="meeting-time-date-container">
-                      <span className="meeting-time">
-                        {formatTime(meeting.time_slot)}
-                      </span>
-                      <span className="meeting-date">{meeting.date}</span>
+        <div className="schedule-view-container">
+          {data
+            .filter((meeting) => isSameDay(new Date(meeting.date), selectedDay))
+            .map((meeting) => (
+              <div key={meeting.id} className="task-card">
+                <ul className="meeting-list">
+                  <li className="meeting-item">
+                    <div className="meeting-details">
+                      <div className="meeting-time-date-container">
+                        <span className="meeting-time">
+                          {formatTime(meeting.time_slot)}
+                        </span>
+                        <span className="meeting-date">{meeting.date}</span>
+                      </div>
+                      <span className="meeting-service">{meeting.service}</span>
+                      <button
+                        onClick={() => handleDeleteAppointment(meeting.id)}
+                        className="delete-meeting-button"
+                      >
+                        G
+                      </button>
                     </div>
-
-                    <span className="meeting-service">{meeting.service}</span>
-                    <button
-                      onClick={() => handleDeleteAppointment(meeting.id)}
-                      className="delete-meeting-button"
-                    >
-                      G
-                    </button>
-                  </div>
-                  <div className="breakpoint">
-                    <hr />
-                  </div>
-                  {/* <div className="meeting-notes-container">
-                    <span className="meeting-notes">{meeting.notes}</span>
-                  </div> */}
-                  <div className="meeting-client-container">
-                    <span className="meeting-client-name">
-                      {meeting.full_name}
-                    </span>
-                    <span className="meeting-client-email">
-                      {meeting.email}
-                    </span>
-                    <span className="meeting-location">{meeting.location}</span>
-                  </div>
-                </li>
-              ))}
-          </ul>
+                    <div className="breakpoint">
+                      <hr />
+                    </div>
+                    <div className="meeting-client-container">
+                      <span className="meeting-client-name">
+                        {meeting.full_name}
+                      </span>
+                      <span className="meeting-client-email">
+                        {meeting.email}
+                      </span>
+                      <span className="meeting-location">
+                        {meeting.location}
+                      </span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            ))}
         </div>
       </div>
     </div>
