78b5598d9c1ff1fc2e50a7166af46eb7aafdb7ee
kidmpukane <lwandisozwane91@gmail.com>
2024-06-19 07:43:00 +0200

    basic form styling complete

--- src/pages/AvailabilityEditPage.jsx
+++ src/pages/AvailabilityEditPage.jsx
@@ -70,109 +70,111 @@ function AvailabilityEditPage() {
   ];
 
   return (
-    <div className="availability-registration-container">
-      {isLoading && <p>Submitting availability...</p>}
-      {isError && <p>Error submitting availability: {error.message}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="full-width">
-          <label>Exclude Months:</label>
-          <select name="exclude_months" onChange={handleDateChange} value="">
-            <option value="" disabled>
-              Select a month to exclude
-            </option>
-            {monthOptions.map((month) => (
-              <option key={month.value} value={month.value}>
-                {month.label}
+    <div className="availability-parent-container">
+      <div className="availability-registration-container">
+        {isLoading && <p>Submitting availability...</p>}
+        {isError && <p>Error submitting availability: {error.message}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="full-width">
+            <label>Exclude Months:</label>
+            <select name="exclude_months" onChange={handleDateChange} value="">
+              <option value="" disabled>
+                Select a month to exclude
               </option>
-            ))}
-          </select>
-          <div>
-            {formData.exclude_months.map((month, index) => (
-              <span key={index} className="selected-month">
-                {monthOptions.find((m) => m.value === parseInt(month)).label}
-              </span>
-            ))}
+              {monthOptions.map((month) => (
+                <option key={month.value} value={month.value}>
+                  {month.label}
+                </option>
+              ))}
+            </select>
+            <div>
+              {formData.exclude_months.map((month, index) => (
+                <span key={index} className="selected-month">
+                  {monthOptions.find((m) => m.value === parseInt(month)).label}
+                </span>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="full-width">
-          <label>Exclude Particular Days:</label>
-          <input
-            type="date"
-            name="exclude_particular_days"
-            onChange={handleDateChange}
-            value=""
-          />
-          <div>
-            {formData.exclude_particular_days.map((day, index) => (
-              <span key={index} className="selected-day">
-                {new Date(day).toLocaleDateString()}
-              </span>
-            ))}
-          </div>
-        </div>
-        <div>
-          <label>
+          <div className="full-width">
+            <label>Exclude Particular Days:</label>
             <input
-              type="checkbox"
-              name="exclude_weekends"
-              checked={formData.exclude_weekends}
+              type="date"
+              name="exclude_particular_days"
+              onChange={handleDateChange}
+              value=""
+            />
+            <div>
+              {formData.exclude_particular_days.map((day, index) => (
+                <span key={index} className="selected-day">
+                  {new Date(day).toLocaleDateString()}
+                </span>
+              ))}
+            </div>
+          </div>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                name="exclude_weekends"
+                checked={formData.exclude_weekends}
+                onChange={handleChange}
+              />
+              Exclude Weekends
+            </label>
+          </div>
+          <div>
+            <label>Start Hour:</label>
+            <input
+              type="time"
+              name="start_hour"
+              value={formData.start_hour}
               onChange={handleChange}
             />
-            Exclude Weekends
-          </label>
-        </div>
-        <div>
-          <label>Start Hour:</label>
-          <input
-            type="time"
-            name="start_hour"
-            value={formData.start_hour}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>End Hour:</label>
-          <input
-            type="time"
-            name="end_hour"
-            value={formData.end_hour}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Location:</label>
-          <input
-            type="text"
-            name="location"
-            value={formData.location}
-            onChange={handleChange}
-            placeholder="Enter your location"
-          />
-        </div>
-        <div>
-          <label>Service Offered:</label>
-          <select
-            name="service_offered"
-            value={formData.service_offered}
-            onChange={handleChange}
-          >
-            <option value="Consultation">Consultation</option>
-            <option value="Follow-up">Follow-up</option>
-            <option value="Therapy">Therapy</option>
-          </select>
-        </div>
-        <div>
-          <label>Timezone:</label>
-          <input
-            type="text"
-            name="timezone"
-            value={formData.timezone}
-            onChange={handleChange}
-            placeholder="e.g. UTC"
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+          </div>
+          <div>
+            <label>End Hour:</label>
+            <input
+              type="time"
+              name="end_hour"
+              value={formData.end_hour}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label>Location:</label>
+            <input
+              type="text"
+              name="location"
+              value={formData.location}
+              onChange={handleChange}
+              placeholder="Enter your location"
+            />
+          </div>
+          <div>
+            <label>Service Offered:</label>
+            <select
+              name="service_offered"
+              value={formData.service_offered}
+              onChange={handleChange}
+            >
+              <option value="Consultation">Consultation</option>
+              <option value="Follow-up">Follow-up</option>
+              <option value="Therapy">Therapy</option>
+            </select>
+          </div>
+          <div>
+            <label>Timezone:</label>
+            <input
+              type="text"
+              name="timezone"
+              value={formData.timezone}
+              onChange={handleChange}
+              placeholder="e.g. UTC"
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
